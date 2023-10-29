7baee79dc2fb8c40c14517027ca088382b3ed243
omar <o.wakeel@intouch.com>
2023-10-29 12:08:09 +0200

    feat: adjust container config

---
+++ container/src/bootstrap.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+const el = document.querySelector('#root')
+
+console.log({ el });
+
+ReactDOM.render((<App />, el))
